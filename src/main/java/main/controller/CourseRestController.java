630efd4575d35dd175c9f9a389733d2046ca66eb
IvanRom123 <ivan.gopkins@gmail.com>
2023-03-14 21:24:32 +0100

    hi

---
+++ src/main/java/main/controller/CourseRestController.java
@@ -0,0 +1,40 @@
+package main.controller;
+
+
+import main.model.entities.Course;
+import main.model.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class CourseRestController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @PostMapping("/name")
+    public ResponseEntity<?> CourseByName(@RequestParam String name){
+        Course savedCourse = courseRepository.findAll().stream()
+                    .filter(s -> s.getName().equals(name)).findAny().get();
+            if (savedCourse != null) {
+                return new ResponseEntity<>(name,HttpStatus.OK);
+            }
+            return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(null);
+    }
+    @PostMapping("/description")
+    public ResponseEntity<?> CourseByDescription(@RequestParam String description) {
+        Course savedCourse = courseRepository.findAll().stream()
+                .filter(s -> s.getName().equals(description)).findAny().get();
+        if (savedCourse != null) {
+            return new ResponseEntity<>(description,HttpStatus.OK);
+        }
+        return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(null);
+    }
+}
