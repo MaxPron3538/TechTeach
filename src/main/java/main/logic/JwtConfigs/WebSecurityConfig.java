5d46f9ff86c3c48f95c89e7f01c263668e7331eb
Maxim <maxpron1325@gmail.com>
2023-03-24 19:31:26 +0200

    Updating tsetController

--- src/main/java/main/logic/JwtConfigs/WebSecurityConfig.java
+++ src/main/java/main/logic/JwtConfigs/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/signUp","/signIn","tests/**").permitAll().
+                .authorizeRequests().antMatchers("/signUp","/signIn","/tests","tests/**").permitAll().
                         anyRequest().authenticated().and().
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
